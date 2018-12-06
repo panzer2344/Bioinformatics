ed33ed0c76a9365aa0a9f6b3f7aad54d57fcaf7f
panzer2344 <lexathebest23@gmail.com>
2018-12-06 12:20:43 +0300

    create 3.2

---
+++ 3.2_CyclopeptideScoring.py
@@ -0,0 +1,24 @@
+
+
+masses = {
+	'A' : 71, 'R' : 156, 'N' : 114, 'D' : 115, 'C' : 103, 'E' : 129, 'Q' : 128, 'G' : 57, 'H' : 137, 
+	'I' : 113, 'L' : 113, 'K' : 128, 'M' : 131, 'F' : 147, 'P' : 97, 'S' : 87, 'T' : 101, 'W' : 186, 
+	'Y' : 163, 'V' : 99 
+}
+
+masses_dict = sorted(set(masses.values()))
+
+
+
+def scoring():
+	
+
+
+def main():
+	peptide = input()
+	spectrum = [int(elem) for elem in input().split()]
+
+
+
+if __name__ == "__main__":
+	main()
